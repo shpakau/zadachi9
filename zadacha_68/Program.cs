c6f95d676079d0d27d20efe1140bb15df79b901f
shpakau <shpakau@yahoo.com>
2022-09-19 19:10:19 +0300

    zadacha_68

---
+++ zadacha_68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД)
+//  этих чисел с помощью рекурсии.
+// M = 28; N = 7 -> 7
+
+#include <iostream>
+using namespace std;
+ int GCD(int a, int b)
+{while (a - b != 0)
+ {
+   if (a > b) a -= b;
+   else b -= a;
+ }
+return a;  
+}
+ 
+int main()
+{
+ 
+    int a, b;
+    cout << "Введите два целых числа:" << endl;
+    cin >> a >> b;
+    cout << "НОД для 2-x введенных чисел: " << GCD(a, b) << endl;
+    system("pause");
+    return 0;
+}
